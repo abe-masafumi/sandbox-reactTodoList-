747ae80fd6058cdd7d8b7d395f503882ee249b37
abe-masafumi <83285963+abe-masafumi@users.noreply.github.com>
2021-08-14 03:11:44 +0900

    5個以上登録できなくする機能追加

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
+// const style = {
+//     background: "#c1ffff"
+//     width: "var(--w)",
+//     height: "30px",
+//     padding: "var(--p)",
+//     margin: "var(--m)",
+//     border-radius: "var(--border-r)"
+// }
+
 export const InputTodo = (props) => {
-  const { todoText, onChange, onClick } = props;
+  const { todoText, onChange, onClick, disabled } = props;
   return (
     <div className="input-area">
       <input
@@ -9,8 +18,11 @@ export const InputTodo = (props) => {
         placeholder="TODOを入力"
         value={todoText}
         onChange={onChange}
+        disabled={disabled}
       />
-      <button onClick={onClick}>追加</button>
+      <button disabled={disabled} onClick={onClick}>
+        追加
+      </button>
     </div>
   );
 };
